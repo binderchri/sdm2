2457b4db5bd1010e824a0b258447ddd704c5d535
Christian Binder <binderchri@gmail.com>
2017-04-23 01:03:29 +0200

    initial version

---
+++ getnonnumericcolumns.py
@@ -0,0 +1,45 @@
+file = "/home/binderchri/kddcup1999/kddcup.data.corrected"
+delim = ","
+
+
+# http://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-float-in-python
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+invalidColumns = list()
+countOfColumns = 0
+
+with open(file) as f:
+    for line in f.readlines():
+        splitted = line.split(delim)
+        if len(splitted) > countOfColumns:
+            countOfColumns = len(splitted)
+
+        for idx, val in enumerate(splitted):
+            if not is_number(val) and not idx in invalidColumns:
+                invalidColumns.append(idx)
+
+print("total columns: ", countOfColumns)
+print("non-numeric columns, zero-based:", invalidColumns)
+
+validColumns = [i for i in range(countOfColumns) if i not in invalidColumns]
+print("numeric columns, zero-based:", validColumns)
+
+# https://unix.stackexchange.com/questions/222121/how-to-remove-a-column-or-multiple-columns-from-file-using-shell-command
+cutcommand = "cut -f{} -d\"{}\" {}".format(",".join([str(i+1) for i in validColumns]),
+                                       delim,
+                                       file)
+print(cutcommand)
+
+# results:
+# non-numeric columns, zero-based: [1, 2, 3, 41]
+
+#total columns:  42
+#non-numeric columns, zero-based: [1, 2, 3, 41]
+#numeric columns, zero-based: [0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+#cut -f1,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41 -d"," /tmp/ass2/test.txt
---
+++ kmeans.py
@@ -0,0 +1,49 @@
+# Parts of this code are taken from:
+#   https://spark.apache.org/docs/2.1.0/mllib-clustering.html#k-means
+
+from pyspark import SparkContext, SparkConf
+from pyspark.mllib.clustering import KMeans, KMeansModel
+from numpy import array
+from math import sqrt
+from time import time
+
+conf = SparkConf()
+sc = SparkContext()
+
+file = "/home/binderchri/kddcup1999/kddcup.data.cleaned"
+
+data = sc.textFile(file)
+parsedData = data.map(lambda line: array([float(x) for x in line.split(',')]))
+
+ks = [1,2,3,4,5,8,13,20,50,100,200,500]
+#ks = [1,3,5,10,50,200,1000] # for performance measurement
+
+for k in ks:
+    startTime = time()
+
+    epsilon = 1e-6
+
+    clusters = KMeans.train(parsedData,
+                            k=k,
+                            #epsilon=epsilon,
+                            #maxIterations=1000,
+                            #seed=99
+                           )
+
+    elapsed = time() - startTime # seconds
+
+    # Evaluate clustering by computing Within Set Sum of Squared Errors
+    def error(point):
+        center = clusters.centers[clusters.predict(point)]
+        return sqrt(sum([x**2 for x in (point - center)]))
+
+    WSSSE = 0
+    # for performance measurement omit error estimation (very time consuming step)
+    WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+
+    output = "k={}, error={}, duration={} seconds, epsilon={}".format(k, WSSSE, elapsed, epsilon)
+
+    with open("/home/binderchri/kddcup1999/out.txt", "a") as f:
+        print(output, file=f)
+
+sc.stop()
